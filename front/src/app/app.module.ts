0907cd6e172881958eb6c5ad88b83f4b105cefea
Marcelo Carro <16marcelo1bac@gmail.com>
2022-10-11 20:52:28 +0200

    front - Subir archivo

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './modules/test/test.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { TestComponent } from './modules/test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    HttpClientModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/modules/test/test.component.ts
+++ front/src/app/modules/test/test.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TestService } from '../services/test.service';
+
 
 @Component({
   selector: 'app-test',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  fileName = '';
+  file: File;
+
+  constructor(private readonly testService: TestService) { }
 
   ngOnInit(): void {
+  }
+
+  onFileSelected(event) {
+    this.file = event.target.files[0];
+
+    if (this.file) {
+      this.fileName = this.file.name;
+
+      // const formData = new FormData();
+      // formData.append("thumbnail", file);
+      // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+      // upload$.subscribe();
+
+      console.log('nombreFich', this.fileName);
+    }
+  }
+
+  uploadFile() {
+    console.log('fich', this.file);
+    this.testService.uploadFile(this.file);
   }
 
 }
