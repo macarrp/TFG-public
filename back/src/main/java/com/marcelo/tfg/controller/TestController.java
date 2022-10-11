f7967cb82fe8545c4ce7e74c701dc41fd19b9d5b
Marcelo Carro <16marcelo1bac@gmail.com>
2022-10-11 20:53:40 +0200

    back - Transformación completa

--- back/src/main/java/com/marcelo/tfg/controller/TestController.java
+++ back/src/main/java/com/marcelo/tfg/controller/TestController.java
@@ -1,16 +1,23 @@
 package com.marcelo.tfg.controller;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
+import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.logging.LogLevel;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -18,43 +25,43 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/test")
 public class TestController {
 
+	@Operation(summary = "Test kettle")
 	@PostMapping("/kettle")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public void testKettle(@RequestParam("file") MultipartFile file) {
 
 		File kettleFileDirectory = new File("src/main/kettle-jobs");
-		
-		log.info(file.getName());
-		
-//		File kettleJob = null;
-//		try {
-//			kettleJob = new File(kettleFileDirectory.getCanonicalFile() + "/sigpeac_usuarios_normal.ktr");
-//			
-//		} catch (IOException e) {
-//			log.error("Error al recuperar el archivo");
-//		}
-		
-//		return kettleJob != null && kettleJob.exists() ? "Si" : "No";
+		File temp = null;
 
-//		return kettleFileDirectory.getAbsolutePath();
+		log.info(file.getOriginalFilename());
 
-//		try {
-//			KettleEnvironment.init();
-//            TransMeta metaData = new TransMeta("sigpeac_usuarios_normal.ktr");
-//            Trans trans = new Trans(metaData);            
-//            trans.setLogLevel(LogLevel.BASIC);
-//            trans.execute(null);
-//            trans.waitUntilFinished();
-//            if (trans.getErrors() > 0) {
-//                log.error("Error Ejecutando la transformacion");
-//                return "Error Ejecutando la transformacion";
-//            }
-//            
-//            return "Transformacion ejecutada con éxito";
-//        } catch (KettleException e) {
-//            log.error("Error ejecutando transf", e);
-//            return "Error Ejecutando la transformacion";
-//        }
+		try {
+			temp = new File(kettleFileDirectory.getCanonicalPath() + File.separator + file.getOriginalFilename());
+
+			file.transferTo(temp);
+		} catch (IllegalStateException e) {
+			log.error("Error sobre el estado del documento", e);
+		} catch (IOException e) {
+			log.error("Error sobre el estado del documento", e);
+		}
+
+		try {
+			executeKettleTransformation(temp);
+		} catch (KettleException e) {
+			log.error("Error ejecutando la transformacion", e);
+		}
+	}
+
+	private void executeKettleTransformation(File kettleJob) throws KettleException {
+		KettleEnvironment.init();
+		TransMeta metaData = new TransMeta(kettleJob.getPath());
+		Trans trans = new Trans(metaData);
+		trans.setLogLevel(LogLevel.BASIC);
+		trans.execute(null); // <-- Arguments, variables de la transformacion?
+		trans.waitUntilFinished();
+		if (trans.getErrors() > 0) {
+			log.error("Error ejecutando la transformacion");
+		}
 	}
 
 }
