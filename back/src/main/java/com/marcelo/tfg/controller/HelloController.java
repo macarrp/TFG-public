6d83911c862f4e8b55ea6a7decac9a04d9515ebd
Marcelo Carro <16marcelo1bac@gmail.com>
2022-10-26 22:52:59 +0200

    Añadir HelloController.java

---
+++ back/src/main/java/com/marcelo/tfg/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.marcelo.tfg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String index() {
+		return "Servidor inicializado";
+	}
+
+}
