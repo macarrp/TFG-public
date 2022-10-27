fde3e25c3e582cb25ecd45adef8f33706ba97eda
Marcelo Carro <16marcelo1bac@gmail.com>
2022-10-27 22:34:01 +0200

    Modificar Springboot para despliegue

--- back/src/main/java/com/marcelo/tfg/TfgApplication.java
+++ back/src/main/java/com/marcelo/tfg/TfgApplication.java
@@ -2,12 +2,19 @@ package com.marcelo.tfg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TfgApplication {
+public class TfgApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TfgApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TfgApplication.class);
+    }
 
 }
