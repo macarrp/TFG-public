7625a97b2d1529d15ed7a39466b4bb2a6152ff67
Marcelo Carro <16marcelo1bac@gmail.com>
2023-02-21 20:43:22 +0100

    main - Quitar AUD

--- back/src/main/java/com/marcelo/tfg/provider/KettleAudProvider.java
+++
@@ -1,8 +0,0 @@
-package com.marcelo.tfg.provider;
-
-import java.io.File;
-
-public interface KettleAudProvider {
-
-	boolean registrarTransformacion(File ktr, String logsTransformacion) throws Exception;
-}
--- back/src/main/java/com/marcelo/tfg/repository/KettleAudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marcelo.tfg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.marcelo.tfg.repository.entity.KettleAudEntity;
-
-public interface KettleAudRepository extends JpaRepository<KettleAudEntity, Long>{
-
-}
